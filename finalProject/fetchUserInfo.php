909569ac38eda0b42fe5cbc84cd4ab62be6fdd97
Yasmine Roy <yasroy11@yahoo.com>
2018-11-21 10:52:47 -0500

    Parse JSON now functioning
    The next step is to assign all the usernames to the shapes in the node array.

--- finalProject/fetchUserInfo.php
+++ finalProject/fetchUserInfo.php
@@ -25,13 +25,10 @@ $result = $db->query($sql_select);
 if (!$result) die("Cannot execute query.");
 while($row = $result->fetchArray(SQLITE3_NUM))
 {
-  echo($row[0]);
+  //echo($row[0]);
   if($row[0] === 0)
   {
     $queryInsert ="INSERT INTO userInfo(username)VALUES ('$uname')";
-    $uAmnt = $_GET["userAmount"];
-    $uAmnt = "SELECT userInfo(username)VALUES ('$uname')";
-    echo('$uAmnt');
 
   // again we do error checking when we try to execute our SQL statement on the db
 	 $ok1 = $db->exec($queryInsert);
@@ -41,11 +38,31 @@ while($row = $result->fetchArray(SQLITE3_NUM))
     exit;
     }
     //send back success...
-    echo("insertion complete");
-    exit;
+  //  echo("insertion complete");
+
+  //  echo($uAmnt);
+  //  exit;
 
   }
-  echo("already taken");
+  //echo("already taken");
+  $uAmnt = "SELECT username FROM userInfo";
+  $result = $db->query($uAmnt);
+      if (!$result) die("Cannot execute query.");
+
+      // get a row...
+      // MAKE AN ARRAY::
+      $res = array();
+      $i=0;
+      while($row = $result->fetchArray(SQLITE3_ASSOC))
+      {
+        // note the result from SQL is ALREADy ASSOCIATIVE
+        $res[$i] = $row;
+        $i++;
+      }//end while
+      // endcode the resulting array as JSON
+      $myJSONObj = json_encode($res);
+      echo $myJSONObj;
+      exit;
 
 }
  exit;
