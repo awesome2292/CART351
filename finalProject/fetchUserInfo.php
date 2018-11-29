1f12a70fd436cbdd0396563ab0aebe15c86eb20d
Yasmine <yasroy11@yahoo.com>
2018-11-29 14:58:12 -0500

    Trying to generate a random integer in the xCoord column

--- finalProject/fetchUserInfo.php
+++ finalProject/fetchUserInfo.php
@@ -21,6 +21,9 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
 // need to process
 $uname= $_POST['u_name'];
 $sql_select="SELECT COUNT(username) FROM userInfo WHERE username = '$uname'";
+$xval=$_POST['x_val'];
+$sql_xval="SELECT COUNT(xCoord) FROM userInfo WHERE xCoord = '$xval'"
+
 $result = $db->query($sql_select);
 if (!$result) die("Cannot execute query.");
 while($row = $result->fetchArray(SQLITE3_NUM))
@@ -28,7 +31,8 @@ while($row = $result->fetchArray(SQLITE3_NUM))
   //echo($row[0]);
   if($row[0] === 0)
   {
-    $queryInsert ="INSERT INTO userInfo(username)VALUES ('$uname')";
+    $queryInsert ="INSERT INTO userInfo(username,xCoord)VALUES ('$uname')";
+    $xval = rand(100,1000);
 
   // again we do error checking when we try to execute our SQL statement on the db
 	 $ok1 = $db->exec($queryInsert);
