5742f52323744f24412e2bfef44566e07af7efbb
Yasmine Roy <yasroy11@yahoo.com>
2018-12-03 14:56:40 -0500

    Added lines and changing size for the circles
    Trying to change weight of the lines now

--- finalProject/script.js
+++ finalProject/script.js
@@ -6,6 +6,13 @@ var assoc = [];
 var xCoord = [];
 var yCoord = [];
 var currentUser;
+var currentUserX;
+var currentUserY;
+var currentUserR;
+var connectionLine;
+var nodeClicked = false;
+var numClicks = 0;
+var nodeColor;
 
 var displayNodes = false;
 
@@ -53,34 +60,59 @@ function createNode(){
 
   for(var i = 0; i < users.length; i++){
     if(users[i].username === userName){
-      currentUser = new Node(users[i].xCoord,users[i].yCoord,ellipseSize,users[i].username, color(255,0,0));
+      nodeColor = color(255,0,0);
+      currentUserX = users[i].xCoord;
+      currentUserY = users[i].yCoord;
+      currentUser = new Node(currentUserX,currentUserY,ellipseSize,users[i].username, nodeColor, numClicks);
       console.log("The current user is " + users[i].username);
     }
     else{
-    nodes.push(new Node(users[i].xCoord,users[i].yCoord,ellipseSize,users[i].username, color(255)));
+      nodeColor = color(255);
+    nodes.push(new Node(users[i].xCoord,users[i].yCoord,ellipseSize,users[i].username, nodeColor, numClicks));
   }
   }
 }
 
-function Node(x,y,r,user,clr){
+function Node(x,y,r,user,clr,clicks,clicked){
   this.x = x;
   this.y = y;
   this.r =r;
   this.user = user;
   this.clr = clr;
+  this.clicks = clicks;
+  this.clicked = clicked;
   console.log(this.user);
 
-  this.display = function(){
+  this.display = function(strkWeight){
+    this.strokeWeight = strkWeight;
     fill(this.clr);
     ellipse(this.x, this.y,this.r,this.r);
+    if(this.clicked){
+    this.strkWeight +=1;
+    stroke(255);
+    strokeWeight(this.strkWeight);
+    line(currentUserX, currentUserY, this.x, this.y);
+    //nodeClicked = false;
+    }
   }
 
   this.clickNode = function(){
+
       if(Math.sqrt(Math.pow(this.x-mouseX,2)+Math.pow(this.y-mouseY,2)) < this.r){
          console.log("This node is clicked");
+         this.clicked = true;
+         this.clicks+=1;
+         console.log(this.clicks);
+         this.r=this.r*(this.clicks/100+1);
+         console.log(this.r);
 
          }
        }
+
+  // this.drawLine = function(){
+  //   fill(this.clr);
+  //   connectionLine = line(currentUserX, currentUserY, this.x, this.y);
+  // }
 
 
 }
@@ -89,10 +121,4 @@ function mousePressed(){
   for(var i = 0; i < nodes.length; i++){
     nodes[i].clickNode();
   }
-}
-
-
-
-function drawLine(){
-
 }
